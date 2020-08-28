8f383cffb9ac2a347806b5ccc262c4f27c3fa84d
s01196702 <50027874+s01196702@users.noreply.github.com>
2020-08-28 10:01:03 -0400

    Add files via upload

---
+++ Lab1/Parajuli-Lab1.php
@@ -0,0 +1,21 @@
+<?php
+
+   // PHP code to initialize variables
+   $message = "<span class='bluetext'>This is PHP.</span>";
+
+?>
+<html lang="en">
+<head>
+   <style>
+      .bluetext { color:blue; }
+   </style>
+   <title>IT 2600</title>
+</head>
+<body>
+   <h2>Arju Parajuli - IT 2600 - Lab 1</h2>
+   
+   <p>This is HTML</p>
+   
+   <p>The message is: <?php echo $message; ?></p>
+</body>
+</html>
